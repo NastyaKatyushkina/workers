f80cea0ebd57b3e40513a27b759094e82236d878
NastyaKatyushkina <nastyusha_kat@mail.ru>
2023-04-16 20:38:03 +0500

    workers

---
+++ index.js
@@ -0,0 +1,13 @@
+const serverEngine = require('./server');
+
+const port = process.env.PORT || 3000;
+
+async function start() {
+  try {
+    serverEngine.listen(port, () => console.log(`Server run on ${port} port.`));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+start();
--- server.js
+++ server.js
@@ -1,59 +1,36 @@
-const http = require('http');
 const Koa = require('koa');
-const Router = require('koa-router');
-const koaBody = require('koa-body');
 const cors = require('@koa/cors');
+const koaBody = require('koa-body');
 const slow = require('koa-slow');
-const faker = require('faker');
+const Router = require('koa-router');
+const fakeData = require('./fakeData');
+
+const serverEngine = new Koa();
 const router = new Router();
-const app = new Koa();
-const port = process.env.PORT || 7070;
-const server = http.createServer(app.callback());
 
-app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-    'Access-Control-Allow-Origin': true,
-    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
-  })
-);
+serverEngine
+  .use(
+    cors({
+      origin: '*',
+      allowMethods: ['GET', 'OPTIONS'],
+    })
+  )
+  .use(koaBody({ urlencoded: true, multipart: true }))
+  .use(
+    slow({
+      delay: 3000,
+    })
+  );
 
-app.use(
-  koaBody({
-    text: true,
-    urlencoded: true,
-    multipart: true,
-    json: true,
-  })
-);
-
-app.use(
-  slow({
-    delay: 5000,
-  })
-);
-
-router.get('/news/arts&culture', async (ctx) => {
-  function createNews() {
-    return {
-      id: faker.datatype.uuid(),
-      image: faker.image.abstract(70, 70),
-      text: faker.lorem.sentence(8),
-      received: Date.now(),
-    };
-  }
-
-  const quantity = 3;
-  const news = Array.from({ length: quantity }, createNews);
-
-  ctx.response.body = JSON.stringify({
+router.get('/news', async (ctx) => {
+  const news = [fakeData(), fakeData(), fakeData()];
+  ctx.response.body = {
     status: 'ok',
-    timestamp: Date.now(),
     news,
-  });
+    timestamp: new Date(),
+  };
 });
 
-app.use(router.routes()).use(router.allowedMethods());
+serverEngine.use(router.routes()).use(router.allowedMethods());
 
-server.listen(port, () => console.log('Server started'));
+module.exports = serverEngine;
